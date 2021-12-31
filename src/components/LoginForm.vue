a736748dc457268810f9df9d592282a114548d3a
rectanglehihat <rectanglehihat@gmail.com>
2021-12-31 10:09:37 +0900

    login api 함수 생성

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,19 +1,33 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="text" />
+      <input id="password" type="text" v-model="password" />
     </div>
     <button>로그인</button>
   </form>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      axios.post();
+    },
+  },
+};
 </script>
 
 <style></style>

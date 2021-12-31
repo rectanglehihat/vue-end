7688c59df86feb48f13f3a8448bc43e0cff34377
rectanglehihat <rectanglehihat@gmail.com>
2021-12-31 10:02:35 +0900

    login form 컴포넌트 생성 및 마크업

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="text" />
+    </div>
+    <button>로그인</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

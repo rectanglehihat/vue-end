a1fba4356a8dcb45ef863d85db802a42d51bf7ad
rectanglehihat <rectanglehihat@gmail.com>
2022-01-03 14:56:48 +0900

    LoadingSpinner : 로딩 상태 표시

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,23 +2,13 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
         <PostListItem
           v-for="postItem in postItems"
           :key="postItem._id"
           :postItem="postItem"
         ></PostListItem>
-        <!-- <li v-for="postItem in postItems" :key="postItem._id">
-          <div class="post-title">
-            {{ postItem.title }}
-          </div>
-          <div class="post-contents">
-            {{ postItem.contents }}
-          </div>
-          <div class="post-time">
-            {{ postItem.createdAt }}
-          </div>
-        </li> -->
       </ul>
     </div>
   </div>
@@ -27,19 +17,24 @@
 <script>
 import { fetchPosts } from "@/api/index";
 import PostListItem from "@/components/posts/PostListItem.vue";
+import LoadingSpinner from "@/components/common/LoadingSpinner.vue";
 
 export default {
   components: {
     PostListItem,
+    LoadingSpinner,
   },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchData() {
+      this.isLoading = true;
       const { data } = await fetchPosts();
+      this.isLoading = false;
       console.log(data.posts);
       this.postItems = data.posts;
     },

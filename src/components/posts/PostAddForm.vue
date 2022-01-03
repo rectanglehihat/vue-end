1acbef28878c860f0c3dc2583a2c76090cb4c755
rectanglehihat <rectanglehihat@gmail.com>
2022-01-03 15:56:21 +0900

    학습노트 등록 폼 데이터 바인딩 & 이벤트 연결

--- src/components/posts/PostAddForm.vue
+++ src/components/posts/PostAddForm.vue
@@ -2,14 +2,14 @@
   <div class="contents">
     <h1 class="page-header">Create Post</h1>
     <div class="form-wrapper">
-      <form class="form">
+      <form class="form" @submit.prevent="submitForm">
         <div>
           <label for="title">Title: </label>
-          <input type="text" id="title" />
+          <input type="text" id="title" v-model="title" />
         </div>
         <div>
           <label for="contents">Contents: </label>
-          <textarea type="text" id="contents" rows="5" />
+          <textarea type="text" id="contents" rows="5" v-model="contents" />
         </div>
         <button class="btn">Create</button>
       </form>
@@ -18,7 +18,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: "",
+      contents: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log("submit");
+    },
+  },
+};
 </script>
 
 <style scoped>

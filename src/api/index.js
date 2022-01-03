3517ba6d3df7c73b1b7cb148b6f50bdabc6c9c66
rectanglehihat <rectanglehihat@gmail.com>
2022-01-03 13:58:26 +0900

    HTTP헤더에 token 넣기 & store에 저장

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,20 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    Authorization: "test1234",
+  },
 });
+
+// const config = {
+//   baseURL: "https://api-nodejs-todolist.herokuapp.com",
+// };
 
 function registerUser(userData) {
   // const url = "http://localhost:3000/signup";
   // return axios.post(url, userData);
   return instance.post("signup", userData);
+  // return axios.post(`${config.baseURL}/user/register`, userData);
 }
 
 function loginUser(userData) {

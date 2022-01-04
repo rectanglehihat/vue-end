5605a9e2dd0ba998cd9ac5399c6f14d6daa4f3e2
rectanglehihat <rectanglehihat@gmail.com>
2022-01-04 13:50:15 +0900

    find 를 이용한 컴포넌트 HTML 요소 검색

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,10 +1,14 @@
-import Vue from "vue";
+import { shallowMount } from "@vue/test-utils";
 import LoginForm from "./LoginForm";
 
 describe("LoginForm.vue", () => {
-  test("컴포넌트가 마운팅되면 username이 존재하고 초기값으로 설정되어 있어야 한다", () => {
-    const instance = new Vue(LoginForm).$mount();
-    console.log(instance.username);
-    expect(instance.username).toBe("");
+  // test("컴포넌트가 마운팅되면 username이 존재하고 초기값으로 설정되어 있어야 한다", () => {
+  //   const wrapper = shallowMount(LoginForm);
+  //   expect(wrapper.vm.username).toBe("");
+  // });
+  test("ID는 이메일 형식이어야 한다", () => {
+    const wrapper = shallowMount(LoginForm);
+    const idInput = wrapper.find("#username");
+    console.log(idInput.html());
   });
 });

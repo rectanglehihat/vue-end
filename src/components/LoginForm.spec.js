e94530e21b4abb0c54b97983cd93aaace8aebdba
rectanglehihat <rectanglehihat@gmail.com>
2022-01-04 13:38:33 +0900

    뷰 컴포넌트 데스트 방법

---
+++ src/components/LoginForm.spec.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import LoginForm from "./LoginForm";
+
+describe("LoginForm.vue", () => {
+  test("컴포넌트가 마운팅되면 username이 존재하고 초기값으로 설정되어 있어야 한다", () => {
+    const instance = new Vue(LoginForm).$mount();
+    console.log(instance.username);
+    expect(instance.username).toBe("");
+  });
+});

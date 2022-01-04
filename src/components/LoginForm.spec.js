9864942c425eaae1dc43532ee6d10c8df9551894
rectanglehihat <rectanglehihat@gmail.com>
2022-01-04 14:26:10 +0900

    로그인 버튼 수정: 폼 형식에 맞지 않을시 비활성화

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -22,5 +22,16 @@ describe("LoginForm.vue", () => {
     expect(warningText.exists()).toBeTruthy();
   });
 
-  test("ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다", () => {});
+  test("ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다", () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: "a@a.com",
+          password: "1234",
+        };
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.element.disabled).toBeTruthy();
+  });
 });
